bc8339e3f120985b1e9fa20d2c5ae4f4d332ff34
ankur2004 <ankur2004@gmail.com>
2014-08-27 19:13:40 +0100

    initial

---
+++ Engine.Test/BaseTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Engine.Test
+{
+    [TestClass]
+    public class BaseTests
+    {
+        [TestMethod]
+        public void TestBlackScholesEuropeanCall()
+        {
+        }
+    }
+}
