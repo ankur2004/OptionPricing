ede706e988e02bbcff7beb7925c6854452781df0
ankur2004 <ankur2004@gmail.com>
2014-08-27 19:14:43 +0100

    initial

--- Engine.Test/BaseTests.cs
+++ Engine.Test/BaseTests.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Engine.Test
+namespace OptionPricing.Engine.Test
 {
     [TestClass]
     public class BaseTests
