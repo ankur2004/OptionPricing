310194f276450a438e77a9b1b2779eabf73985e0
ankur2004 <ankur2004@gmail.com>
2014-08-28 20:22:28 +0100

    initial

--- Engine/Core/Random.cs
+++ Engine/Core/Random.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,22 @@ namespace OptionPricing.Engine.Core
 {
     class Random
     {
+        /// <summary>
+        /// Generates random numbers between 0 and 1.0
+        /// </summary>
+        /// <param name="number">Number of random numbers needed</param>
+        /// <returns></returns>
+        public static List<Double> GetRandomNumers(int number)
+        {
+            var rnd = new System.Random();
+            var rndNumbers = new List<Double>();
+
+            for (int i = 0; i < number; i++)
+            {
+                rndNumbers.Add(rnd.NextDouble());
+            }
+
+            return rndNumbers;
+        }
     }
 }
