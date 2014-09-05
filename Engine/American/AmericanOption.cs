278b1b492cef1f05c1ce1451d15137ec65261c2c
ankur2004 <ankur2004@gmail.com>
2014-09-05 20:21:50 +0100

    american option and tree pricing

---
+++ Engine/American/AmericanOption.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionPricing.Engine.American
+{
+    public class AmericanOption : Option
+    {
+
+        public override OptionType Type
+        {
+            get
+            {
+               return OptionType.American;
+            }
+        }
+    }
+}
