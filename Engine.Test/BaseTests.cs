b9b2c23c62c6c290a23183d9a15ac5391e4968ec
ankur2004 <ankur2004@gmail.com>
2014-09-08 20:05:43 +0100

    Added finite difference test

--- Engine.Test/BaseTests.cs
+++ Engine.Test/BaseTests.cs
@@ -28,6 +28,30 @@ namespace OptionPricing.Engine.Test
             Assert.AreEqual(3.67, blackScholes.Price, 0.01, "Option Price BS");
         }
 
+        /// <summary>
+        /// Calculate and validate delta using Finite Differences
+        /// http://en.wikipedia.org/wiki/Finite_difference 
+        /// </summary>
+        [TestMethod]        
+        public void TestBlackScholesEuropeanCallDelta()
+        {
+            var blackScholes = new BlackScholes(testOption);
+            blackScholes.CalculateOptionPrice();
+
+            var p1 = blackScholes.Price;
+            var testOption2 = testOption.CloneWithBump(OptionInputs.Spot, 0.01);
+
+            IOptionPricer blackScholes2 = new BlackScholes(testOption2);
+            blackScholes2.CalculateOptionPrice();
+            var p2 = blackScholes2.Price;
+
+            var deltaBS = blackScholes.GetDelta();
+            var deltaFD = (p2 - p1)/0.01;
+
+            Assert.AreEqual(deltaBS, deltaFD, 0.01, "Delta");
+        }
+
+
         [TestMethod]
         public void TestMCEuropeanCall()
         {
