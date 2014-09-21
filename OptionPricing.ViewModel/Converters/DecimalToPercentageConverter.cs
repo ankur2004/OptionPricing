50f275e3c67d1dae001b77bb5046757d25b8fbf8
elena83 <lenpetrova@gmail.com>
2014-09-21 10:39:43 +0100

    added percentage converter

--- OptionPricing.ViewModel/Converters/DecimalToPercentageConverter.cs
+++ OptionPricing.ViewModel/Converters/DecimalToPercentageConverter.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 
 namespace OptionPricing.ViewModel.Converters
 {
-    public class BooleanConverter : IValueConverter
+    public class DecimalToPercentageConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            return string.Format(culture, "{0:P2}", value);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            var strValue = value as string;
+            Decimal decimalValue;
+            return Decimal.TryParse(strValue, out decimalValue) ? decimalValue : DependencyProperty.UnsetValue;
         }
     }
 }
